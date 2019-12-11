624a2bf27843c083d674c7c77f0c486ae41a2c76
kramfuller <54829017+kramfuller@users.noreply.github.com>
2019-12-11 20:25:41 +0800

    Update buildec2.tf

--- buildec2.tf
+++ buildec2.tf
@@ -2,13 +2,94 @@ provider "aws" {
     region = "us-west-2"
 }
 
+resource "aws_vpc" "vpc" {
+    cidr_block      = "10.0.0.0/16"
+    instance_tenancy = "default"
 
-resource "fa instance" "web" {
-    ami                  = ""
-    instance type        = "t2.micro"
-    key_name             = "fa_key"
-    associate_public_ip_address = "true"
-    tags {
-        Name = "Future Airline Web"
+    tags = {
+        Name = "main"
+        Location  = "Dubai"
     }
+}
+
+ resource "aws_internet_gateway" "gw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "RT" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/16"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = "${aws_vpc.vpc.id}"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet1"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.vpc.id}"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["180.191.138.11/32"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = ["pl-12c4e678"]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  key_name      = "fa_key"
+  subnet_id     = "$[aws_subnet.public.id]"
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
